e47b1be411a505a3ceb6a1849529ae48ae30f12d
DianaWBor <156772595+DianaWBor@users.noreply.github.com>
2024-01-18 19:13:03 +1300

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,10 +1,11 @@
+package ticTacToe;
 import java.awt.*;
 
 public class Board {
 	// grid line width
 	public static final int GRID_WIDTH = 8;
 	// grid line half width
-	public static final int GRID_WIDHT_HALF = GRID_WIDTH / 2;
+	public static final int GRID_WIDTH_HALF = GRID_WIDTH / 2;
 	
 	//2D array of ROWS-by-COLS Cell instances
 	Cell [][] cells;
@@ -13,8 +14,8 @@ public class Board {
 	public Board() {
 		
 	 //TODO: initialise the cells array using ROWS and COLS constants 
+		//Cell [][] cells;
 
-		
 		for (int row = 0; row < GameMain.ROWS; ++row) {
 			for (int col = 0; col < GameMain.COLS; ++col) {
 				cells[row][col] = new Cell(row, col);
@@ -40,7 +41,8 @@ public class Board {
 		 // check if player has 3-in-that-row
 		if(cells[playerRow][0].content == thePlayer && cells[playerRow][1].content == thePlayer && cells[playerRow][2].content == thePlayer )
 			return true; 
-		
+		if(cells[playerRow][0].content == thePlayer && cells[playerRow][1].content == thePlayer && cells[playerRow][2].content == thePlayer )
+			return true;
 		 // TODO: Check if the player has 3 in the playerCol.
 		 // Hint: Use the row code above as a starting point, remember that it goes cells[row][column] 
 		
@@ -50,14 +52,16 @@ public class Board {
 		if( cells[0][0].content == thePlayer && cells[1][1].content == thePlayer && cells[2][2].content == thePlayer)
 			return true;
 		 
+		// 3-in-the-diagonal in the other direction
+		if( cells[0][2].content == thePlayer && cells[1][1].content == thePlayer && cells[2][0].content == thePlayer)
+			return true;
 		
-		// TODO: Check the diagonal in the other direction
+		 else
 		
-
 		
 		//no winner, keep playing
 		return false;
-	}
+	    }
 	
 	/**
 	 * Draws the grid (rows then columns) using constant sizes, then call on the
@@ -67,12 +71,12 @@ public class Board {
 		//draw the grid
 		g.setColor(Color.gray);
 		for (int row = 1; row < GameMain.ROWS; ++row) {          
-			g.fillRoundRect(0, GameMain.CELL_SIZE * row - GRID_WIDHT_HALF,                
+			g.fillRoundRect(0, GameMain.CELL_SIZE * row - GRID_WIDTH_HALF,                
 					GameMain.CANVAS_WIDTH - 1, GRID_WIDTH,                
 					GRID_WIDTH, GRID_WIDTH);       
 			}
 		for (int col = 1; col < GameMain.COLS; ++col) {          
-			g.fillRoundRect(GameMain.CELL_SIZE * col - GRID_WIDHT_HALF, 0,                
+			g.fillRoundRect(GameMain.CELL_SIZE * col - GRID_WIDTH_HALF, 0,                
 					GRID_WIDTH, GameMain.CANVAS_HEIGHT - 1,                
 					GRID_WIDTH, GRID_WIDTH);
 		}
